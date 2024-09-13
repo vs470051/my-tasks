fa2b019d26dfd7acf806c6e46f55e49a639c10d1
vs470051 <141302929+vs470051@users.noreply.github.com>
2024-09-13 10:23:54 +0530

    Add files via upload

---
+++ jenkinspipeline/src/main/java/com/rushiinfotech/samples/javawarhello/HelloServlet.java
@@ -0,0 +1,13 @@
+package rushiinfotech.in;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/hello", loadOnStartup = 1)
+public class HelloServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        System.out.println("HelloServlet: welcome to LiveApplication :-)");
+    }
+}
---
+++ jenkinspipeline/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Rushi Sample App: success!</title>
+</head>
+<body style="text-align: center">
+<h1>Congratulations!</h1>
+<img src="rushi.jpg">
+
+<h2>Welcome to Home Page!</h2>
+
+
+</body>
+</html>
